3a2747c2209810dd399d1b5cacc51aaf6bf41ae2
0xtito <92818759+0xtito@users.noreply.github.com>
2022-07-09 10:17:31 -0400

    first commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
+const secp = require("@noble/secp256k1");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 3042;
 
 // localhost can have cross origin errors
@@ -8,20 +9,76 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+// bitcoin addresses
+const originalPrivateKeys = {
+  one: secp.utils.randomPrivateKey(),
+  two: secp.utils.randomPrivateKey(),
+  three: secp.utils.randomPrivateKey(),
+};
+
+const originalPublicKeys = {
+  one: secp.getPublicKey(originalPrivateKeys.one),
+  two: secp.getPublicKey(originalPrivateKeys.two),
+  three: secp.getPublicKey(originalPrivateKeys.three),
+};
+
+const keys = {
+  one: {
+    privateKey: convertToPrivateKeyFormat(originalPrivateKeys.one),
+    publicKey: convertToPublicKeyFormat(originalPublicKeys.one),
+  },
+  two: {
+    privateKey: convertToPrivateKeyFormat(originalPrivateKeys.two),
+    publicKey: convertToPublicKeyFormat(originalPublicKeys.two),
+  },
+  three: {
+    privateKey: convertToPrivateKeyFormat(originalPrivateKeys.three),
+    publicKey: convertToPublicKeyFormat(originalPublicKeys.three),
+  },
+};
+
+const accounts = {
+  one: {
+    publicKey: keys.one.publicKey,
+    privateKey: keys.one.privateKey,
+    balance: 100,
+  },
+  two: {
+    publicKey: keys.two.publicKey,
+    privateKey: keys.two.privateKey,
+    balance: 50,
+  },
+  three: {
+    publicKey: keys.three.publicKey,
+    privateKey: keys.three.privateKey,
+    balance: 75,
+  },
+};
+
+function convertToPublicKeyFormat(publicKey) {
+  publicKey = Buffer.from(publicKey).toString("hex");
+  return "0x" + publicKey.slice(publicKey.length - 40);
 }
 
-app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
-  const balance = balances[address] || 0;
+function convertToPrivateKeyFormat(privateKey) {
+  return Buffer.from(privateKey).toString("hex");
+}
+
+console.log(accounts);
+
+app.get("/balance/:address", (req, res) => {
+  const { address } = req.params;
+  let balance = 0;
+  for (let i = 0; i < Object.keys(accounts).length; i++) {
+    if (address === Object.values(accounts)[i].publicKey) {
+      balance = Object.values(accounts)[i].balance;
+    }
+  }
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
+app.post("/send", (req, res) => {
+  const { sender, recipient, amount } = req.body;
   balances[sender] -= amount;
   balances[recipient] = (balances[recipient] || 0) + +amount;
   res.send({ balance: balances[sender] });
